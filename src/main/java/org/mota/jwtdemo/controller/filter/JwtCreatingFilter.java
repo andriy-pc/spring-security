57732ae5dc4bd0b3a33f00e1e2820b68669395a3
andriy-pc <48772239+andriy-pc@users.noreply.github.com>
2023-01-12 11:07:02 +0200

    implemented dummy JWT flow without token refresh (#1)
    Co-authored-by: Andrii.Mota <andrii_mota@ext.icicipruamc.com>

--- src/main/java/org/mota/jwtdemo/controller/filter/JwtCreatingFilter.java
+++ src/main/java/org/mota/jwtdemo/controller/filter/JwtCreatingFilter.java
@@ -1,4 +1,4 @@
-package org.mota.jwtdemo.configuration.filter;
+package org.mota.jwtdemo.controller.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
@@ -7,21 +7,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import org.mota.jwtdemo.service.UserService;
+import org.mota.jwtdemo.utils.JwtUtils;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("jwt-old")
 public class JwtCreatingFilter extends UsernamePasswordAuthenticationFilter {
 
   private final AuthenticationManager authenticationManager;
+  private final UserService userService;
 
-  public JwtCreatingFilter(AuthenticationManager authenticationManager) {
+  public JwtCreatingFilter(AuthenticationManager authenticationManager, UserService userService) {
     super(authenticationManager);
     this.authenticationManager = authenticationManager;
+    this.userService = userService;
   }
 
   @Override
@@ -29,7 +36,7 @@ public class JwtCreatingFilter extends UsernamePasswordAuthenticationFilter {
       HttpServletResponse response) throws AuthenticationException {
     UsernamePasswordAuthenticationToken authentication =
         new UsernamePasswordAuthenticationToken(
-            request.getParameter("username"),
+            request.getParameter("login"),
             request.getParameter("password"));
     return authenticationManager.authenticate(authentication);
   }
@@ -37,9 +44,10 @@ public class JwtCreatingFilter extends UsernamePasswordAuthenticationFilter {
   @Override
   protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
       FilterChain chain, Authentication authResult) throws IOException {
+    UserDetails userDetails = userService.loadUserByUsername(request.getParameter("login"));
     Map<String, String> tokens = new HashMap<>();
-    tokens.put("token", "token");
-    tokens.put("refresh_token", "refresh_token");
+    tokens.put("token", JwtUtils.generateToken(userDetails));
+    tokens.put("refresh_token", JwtUtils.generateRefreshToken(userDetails));
     new ObjectMapper().writeValue(response.getOutputStream(), tokens);
   }
 }
